d83347d95cfec7724d34d97009e1b0362324b36e
Андрей Брижак <andrewbrizhak@gmail.com>
2021-11-10 19:10:09 +0800

    Add project files.

---
+++ ProjectFromStackOverflow/Controllers/ContractorController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ProjectFromStackOverflow.Models;
+
+namespace ProjectFromStackOverflow.Controllers
+{
+    public class ContractorController : Controller
+    {
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult ShowContractorGet(int id)
+        {
+            // код для извлечения объекта из БД
+            var contractor = new Contractor();
+            contractor.Id = id;
+            contractor.Name = "Test";
+            return View(contractor);
+            
+        }
+
+        [HttpPost]
+        public IActionResult ShowContractorPost(int id)
+        {
+            return RedirectToAction("ShowContractorGet", id);
+        }
+    }
+}
---
+++ ProjectFromStackOverflow/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ProjectFromStackOverflow.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
